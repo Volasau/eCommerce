a3c0599c07a828d9fada779d4603d6e3a0a8d94c
Ryhor Volasau <G.volosov@gmail.com>
2023-08-10 17:33:06 +0400

    RSSEC-136 feat: add files with objects for logi and registration pages

---
+++ src/pages/logReg/formObjects/loginObj.ts
@@ -0,0 +1,11 @@
+import { InnerForm } from '../formClasses/classForm';
+import { FormBuilder } from '../formClasses/classFormBuilder';
+
+const formId = 'login-form';
+const buttonText = 'Login';
+const email = new InnerForm('Email', 'text', 'email', 'email', 'email-error', 'Enter the e-mail');
+const password = new InnerForm('Password', 'password', 'password', 'password', 'password-error', 'Enter the password');
+const innerForms = [email, password];
+const innerFormList = innerForms.map((elem) => elem.create());
+
+export const login = new FormBuilder(formId, innerFormList, buttonText);
