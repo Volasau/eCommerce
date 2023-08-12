37897ba0a0d6eb735517c0d8f54a479a06f97567
Mikalai Rusakovich <kolyos83@mail.ru>
2023-08-12 22:41:53 +0300

    RSSEC-29 feat: add function startEmailValidation

--- src/pages/logReg/validation/validateThisInput.ts
+++ src/pages/logReg/validation/validateThisInput.ts
@@ -1,24 +1,26 @@
+import { startEmailValidation } from './validationFunction/startEmailValidation';
+
 export function validateThisInput(input: HTMLInputElement, error: HTMLElement) {
     input.addEventListener('input', (event: Event) => {
         const target = event.target as HTMLInputElement;
         if (target.id === 'email') {
             startEmailValidation(target.value, error);
-        } else if (target.id === 'password') {
-            startPasswordValidation(target.value, error);
-        } else if (target.id === 'name' || target.id === 'last-name' || target.id === 'city') {
-            startNameValidation(target.value, error);
-        } else if (target.id === 'birthdate') {
-            startBirthDateValidation(target.value, error);
-        } else if (target.id === 'postcode') {
-            startPostcodeValidation(target.value, error);
+            // } else if (target.id === 'password') {
+            //     startPasswordValidation(target.value, error);
+            // } else if (target.id === 'name' || target.id === 'last-name' || target.id === 'city') {
+            //     startNameValidation(target.value, error);
+            // } else if (target.id === 'birthdate') {
+            //     startBirthDateValidation(target.value, error);
+            // } else if (target.id === 'postcode') {
+            //     startPostcodeValidation(target.value, error);
         }
     });
 
-    input.addEventListener('input', (event) => {
-        const target = event.target as HTMLInputElement;
-        if (target.id === 'country') {
-            checkCountryInput(target, error);
-            chooseCountry(target, error);
-        }
-    });
+    // input.addEventListener('input', (event) => {
+    //     const target = event.target as HTMLInputElement;
+    //     if (target.id === 'country') {
+    //         checkCountryInput(target, error);
+    //         chooseCountry(target, error);
+    //     }
+    // });
 }
