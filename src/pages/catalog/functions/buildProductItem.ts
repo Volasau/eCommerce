5584b212100115daaa8cf7cfa03f6f345499fbd7
Mikalai Rusakovich <kolyos83@mail.ru>
2023-08-29 13:10:45 +0300

    RSSEC-159 feat: add styles to product list

--- src/pages/catalog/functions/buildProductItem.ts
+++ src/pages/catalog/functions/buildProductItem.ts
@@ -9,13 +9,15 @@ export function buildProductItem(prod: IProduct): HTMLDivElement {
     const prodPrice = prod.allVariants[0].prices[0].value.centAmount / 100;
     const descript = prod.description;
     const product = divHTML.getElement('', `${id}`, 'product min') as HTMLDivElement;
+    const imgBlock = divHTML.getElement('', `${id}-imgBlock`, 'imgBlock') as HTMLDivElement;
     const img = imgHTML.getElement('', `${id}-img`, 'product-img min', `${firstIMG}`, `${prodName} image`, '130px');
     const name = divHTML.getElement(`${prodName}`, `${id}-name`, 'name min');
     const price = divHTML.getElement(`${prodPrice} GBP`, `${id}-price`, 'price min');
     const description = divHTML.getElement(`${descript}`, `${id}-description`, 'description min');
     const cartButton = buttonHTML.getElement('', `${id}-cart`, 'cart-but');
     cartButton.innerHTML = `${cartSVG} BUY`;
-    product.append(img, name, price, description, cartButton);
+    imgBlock.append(img);
+    product.append(imgBlock, name, price, description, cartButton);
 
     return product;
 }
