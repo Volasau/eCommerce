a3736b584162c36044ea6ae8a9ab3ab240535e26
Ryhor Volasau <G.volosov@gmail.com>
2023-09-04 15:34:08 +0400

    RSSEC-87 feat: add new interfaces

--- src/pages/profile/interfaces/dataForUpdet.ts
+++ src/pages/profile/interfaces/dataForUpdet.ts
@@ -1,3 +1,5 @@
+// интерфейс и объект для нового адресса
+
 interface INewAdress {
     country: string;
     city: string;
@@ -18,4 +20,56 @@ export const newAdress: INewAdress = {
     shippingDefault: false,
     billing: false,
     shipping: false,
+};
+// интерфейс и объект для смены личной информации
+
+export interface INewIformUser {
+    email: string;
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+}
+
+export const newIformUser: INewIformUser = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    birthDate: '',
+};
+///  интерфейс и объект для удаления адреса
+
+// interface IIdAddressDelete {
+//     id: string;
+// }
+// export const idAddressDelete: IIdAddressDelete = {
+//     id: '',
+// };
+// интерфейс взят для нового адреса и объект для редоктирования адреса
+
+interface IChangeAdress extends INewAdress {
+    id: string;
+}
+
+export const changeAdress: IChangeAdress = {
+    id: '',
+    country: '',
+    city: '',
+    street: '',
+    code: '',
+    billingDefault: false,
+    shippingDefault: false,
+    billing: false,
+    shipping: false,
+};
+
+// интерфейс для нового пароля
+
+interface IChangePassword {
+    passwordOld: string;
+    passwordNew: string;
+}
+
+export const changePassworObj: IChangePassword = {
+    passwordOld: '',
+    passwordNew: '',
 };
