00c3a808e244c99388676da74a7ab81041c35b49
Mikalai Rusakovich <kolyos83@mail.ru>
2023-09-05 12:23:12 +0300

    RSSEC-181 feat: add listeners to price and alpha sortings

--- src/pages/catalog/listeners/sortProducts.ts
+++ src/pages/catalog/listeners/sortProducts.ts
@@ -1,5 +1,7 @@
 import { productForm } from '../../../server/function/productForm';
 import { ProductSort } from '../../../server/sort/sort';
+import { buildProductItem } from '../functions/product/buildProductItem';
+import { IProductResp } from '../interfaces/categoryResponse/categoryResponseInterface';
 
 export function sortByCheap() {
     document.addEventListener('click', async (event) => {
@@ -9,8 +11,16 @@ export function sortByCheap() {
             try {
                 const productSort = new ProductSort();
                 const data = await productSort.sortByPrice();
-                const result = productForm(data);
-                return result;
+                const result: IProductResp[] = productForm(data);
+                console.log(result);
+
+                const prodList = document.getElementById('product-view') as HTMLDivElement;
+                prodList.innerHTML = '';
+                result.forEach((prod) => {
+                    prodList.append(buildProductItem(prod));
+                });
+                const quantity = document.querySelector('.quantity') as HTMLSpanElement;
+                quantity.textContent = `${result.length}`;
             } catch (error) {
                 console.error('Error:', error);
             }
@@ -26,8 +36,15 @@ export function sortByAlphabet() {
             try {
                 const productSort = new ProductSort();
                 const data = await productSort.sortByName();
-                const result = productForm(data);
-                return result;
+                const result: IProductResp[] = productForm(data);
+
+                const prodList = document.getElementById('product-view') as HTMLDivElement;
+                prodList.innerHTML = '';
+                result.forEach((prod) => {
+                    prodList.append(buildProductItem(prod));
+                });
+                const quantity = document.querySelector('.quantity') as HTMLSpanElement;
+                quantity.textContent = `${result.length}`;
             } catch (error) {
                 console.error('Error:', error);
             }
