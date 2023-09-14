5e8ce6c1a2c4fa9a634662545c99e94238c40756
Ryhor Volasau <G.volosov@gmail.com>
2023-09-14 16:51:30 +0400

    RSSEC-129 feat: add basket page

---
+++ src/pages/basket/basket.ts
@@ -0,0 +1,20 @@
+import Page from '../../core/template/page';
+import { createLink } from '../logReg/utils/createLink.utils';
+
+class CartPage extends Page {
+    text: string;
+
+    constructor(id: string) {
+        super(id);
+        this.text = 'Basket';
+    }
+
+    async render() {
+        const title = this.createHeaderTitle(this.text);
+        const catalog = createLink('#/catalog', 'To go to the catalog click here ', 'Catalog📦', '');
+        this._container.append(title, catalog);
+        return this._container;
+    }
+}
+
+export default CartPage;
