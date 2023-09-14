1eba4f4a870c667b9e6f2bdf020522720634c005
Ryhor Volasau <G.volosov@gmail.com>
2023-09-14 16:27:28 +0400

    RSSEC-124 feat: add link rsschool on page aboutus

--- src/pages/aboutus/aboutUs.ts
+++ src/pages/aboutus/aboutUs.ts
@@ -5,6 +5,7 @@ import { aboutInfo } from './data/infoAboutUs';
 import CardRenderer from './formAboutUs';
 import { createElement } from '../profile/createElement';
 import { textDescription } from './data/infoDescription';
+import rsslogo from '../../assets/images/RSS.png';
 
 class AboutPage extends Page {
     text: string;
@@ -17,6 +18,17 @@ class AboutPage extends Page {
     async render(): Promise<HTMLElement> {
         const title = this.createHeaderTitle(this.text);
         const containerInfo = createElement('div', 'aboutus__container');
+        const linkSchool = createElement('p', 'link-school__container');
+        const rssLink = document.createElement('a');
+        rssLink.classList.add('rss__link');
+        rssLink.href = `https://rs.school/index.html`;
+        rssLink.target = '_blank';
+        const rssIcon = document.createElement('img');
+        rssIcon.classList.add('img__rss');
+        rssIcon.src = rsslogo;
+        rssIcon.alt = 'rsschool';
+        rssLink.append(rssIcon);
+        linkSchool.append(rssLink);
 
         const countriCards = createElement('div', 'container__cards');
         aboutInfo.forEach((info) => {
@@ -29,7 +41,15 @@ class AboutPage extends Page {
         const paragraphThree = createElement('p', 'paragraph__conrtainer', textDescription.paragraphThree);
         const conclusion = createElement('p', 'paragraph__conrtainer', textDescription.conclusion);
 
-        containerInfo.append(introduction, paragraphOne, paragraphTwo, paragraphThree, conclusion, countriCards);
+        containerInfo.append(
+            linkSchool,
+            introduction,
+            paragraphOne,
+            paragraphTwo,
+            paragraphThree,
+            conclusion,
+            countriCards
+        );
         // const catalog = createLink('#/catalog', 'To go to the catalog click here ', 'Catalog📦', '');
         this._container.append(title, containerInfo);
         return this._container;
