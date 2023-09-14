71511fd005da8eddd7a7dcd6a54ac0efbb66e038
Ryhor Volasau <G.volosov@gmail.com>
2023-09-14 15:00:30 +0400

    RSSEC-123 feat: add information about our team

---
+++ src/pages/aboutus/data/infoDescription.ts
@@ -0,0 +1,17 @@
+import { IAboutDescription } from '../interface/interfaceDeascription';
+
+export const textDescription: IAboutDescription = {
+    introduction: `
+  This is an impressive story of how your team was formed and successfully tackled the task. Our team was randomly assembled through the app.rs.school, and despite being in different time zones, you managed to work cohesively and harmoniously on the final project.`,
+
+    paragraphOne: `<span class = 'text__name'> Ryhor Volasau</span> - was appointed as the team leader based on the highest score. He played a pivotal role in the project by creating all the sprints and tasks in Jira, populating products in eCommercetools with all the necessary attributes and information, and actively participating in the development of every project page. He also wrote API requests for eCommercetools to edit customer profiles, making a significant contribution to the overall project development.`,
+
+    paragraphTwo: `<span class = 'text__name'> Mikolai Rusakovich</span> - is an invaluable and responsible member of your team. He actively engaged in all project task discussions, contributed significantly to the product catalog, and implemented numerous functionalities on various project pages. Mikolai also successfully implemented field validation for login and registration and conducted code refactoring based on mentor reviews. His dedication to the project and the time he invested are greatly appreciated by the entire team.`,
+
+    paragraphThree: `<span class = 'text__name'> Azimkhan Abdulsatarov</span>- proved to be a responsible and highly valuable team member. His in-depth understanding of eCommercetools documentation streamlined the team's work. Azimkhan provided clear and understandable information to resolve questions related to server requests. He created the majority of requests for working with eCommercetools and also guided all team members on how to correctly populate attributes and product information in eCommercetools.`,
+
+    conclusion: `Each completed task in the project was shared with all team members for review. Only after approval from each member, the task was merged into the main development branch. Your team's dedication to thoroughness and collaboration ensured the timely completion of tasks, and you all take pride in your collective achievements.
+  
+    Together, your development team achieved remarkable results through open communication, daily meetings, and collaborative problem-solving. Whenever a team member faced a challenge or difficulty in solving a task, the entire team was quick to lend a helping hand. This cooperative spirit and dedication to the project were key factors in creating a successful product.<br>
+    Allow me to introduce your team:`,
+};
