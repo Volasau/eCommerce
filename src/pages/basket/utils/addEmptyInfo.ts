e6dfaaca0744708ac9406bd01e2650d2a6201fa9
Mikalai Rusakovich <kolyos83@mail.ru>
2023-09-17 00:13:24 +0300

    RSSEC-117 feat: add link in empty cart message directing to the catalog page

--- src/pages/basket/utils/addEmptyInfo.ts
+++ src/pages/basket/utils/addEmptyInfo.ts
@@ -5,7 +5,8 @@ export function addEmptyInfo(): HTMLDivElement {
     const wrapper = divHTML.getElement('', 'empty-wrap', 'row-wrapper') as HTMLDivElement;
     const imageBlock = divHTML.getElement('', 'empty-image', 'empty-image') as HTMLDivElement;
     const row1 = divHTML.getElement('The cart is empty', 'row1', 'row1') as HTMLDivElement;
-    const row2 = divHTML.getElement('Please, choose a product in the catalog', 'row2', 'row2') as HTMLDivElement;
+    const row2 = divHTML.getElement('', 'row2', 'row2') as HTMLDivElement;
+    row2.innerHTML = 'Please, choose a product in the <a id ="cat-link" href="#/catalog">CATALOG</a>';
     const img = new Image(50, 50);
     img.src = urlImg;
     imageBlock.append(img);
