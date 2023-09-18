0a0c22553955a8aae64f3b5f7ad1fead641c67c0
Ryhor Volasau <G.volosov@gmail.com>
2023-09-18 12:19:13 +0400

    RSSEC-232 feat: add test for interface newInFormUser

---
+++ test/interface/newInFormUser.test.ts
@@ -0,0 +1,32 @@
+import { INewInformUser } from '../../src/pages/profile/interfaces/newInFormUserInterface';
+
+const testNewInformUser: INewInformUser = {
+    email: 'ryhor@gmail.com',
+    firstName: 'Ryhor',
+    lastName: 'Volasau',
+    birthDate: '1990-01-01',
+};
+
+describe('IAboutDescription Interface', () => {
+    it('should have a email property of type string', () => {
+        expect(typeof testNewInformUser.email).toBe('string');
+    });
+
+    it('should have a firstName property of type string', () => {
+        expect(typeof testNewInformUser.firstName).toBe('string');
+    });
+
+    it('should have a lastName property of type string', () => {
+        expect(typeof testNewInformUser.lastName).toBe('string');
+    });
+
+    it('should have a birthDate property of type string', () => {
+        expect(typeof testNewInformUser.birthDate).toBe('string');
+    });
+    it('should not allow email to be a number', () => {
+        expect(typeof testNewInformUser.email).not.toBe('number');
+    });
+    it('should not allow birthDate to be a boolean', () => {
+        expect(typeof testNewInformUser.birthDate).not.toBe('boolean');
+    });
+});
