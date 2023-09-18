9aca6e4082d19dafb0dd68f0ba755198c871a616
Ryhor Volasau <G.volosov@gmail.com>
2023-09-18 12:17:01 +0400

    RSSEC-232 feat: add test for interfceDeascription

---
+++ test/interface/interfaceDeascription.test.ts
@@ -0,0 +1,40 @@
+import { IAboutDescription } from '../../src/pages/aboutus/interface/interfaceDeascription';
+
+const testAboutDescription: IAboutDescription = {
+    introduction: 'introduction',
+    paragraphOne: 'paragraphOne',
+    paragraphTwo: 'paragraphTwo',
+    paragraphThree: 'paragraphThree',
+    conclusion: 'conclusion I am Superman',
+};
+
+describe('IAboutDescription Interface', () => {
+    it('should have a introduction property of type string', () => {
+        expect(typeof testAboutDescription.introduction).toBe('string');
+    });
+
+    it('should have a paragraphOne property of type string', () => {
+        expect(typeof testAboutDescription.paragraphOne).toBe('string');
+    });
+
+    it('should have a paragraphTwo property of type string', () => {
+        expect(typeof testAboutDescription.paragraphTwo).toBe('string');
+    });
+
+    it('should have a paragraphThree property of type string', () => {
+        expect(typeof testAboutDescription.paragraphThree).toBe('string');
+    });
+
+    it('should have a conclusion property of type string', () => {
+        expect(typeof testAboutDescription.conclusion).toBe('string');
+    });
+    it('should not allow introduction to be a number', () => {
+        expect(typeof testAboutDescription.introduction).not.toBe('number');
+    });
+    it('should not allow paragraphTwo to be a number', () => {
+        expect(typeof testAboutDescription.paragraphTwo).not.toBe('number');
+    });
+    it('should not allow conclusion to be a boolean', () => {
+        expect(typeof testAboutDescription.conclusion).not.toBe('boolean');
+    });
+});
