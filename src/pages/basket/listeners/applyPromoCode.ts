c5553f9e12306e7532d6f3111c612b9d7ec2359a
Azimkhan93 <aabdulsatarov@gmail.com>
2023-09-18 23:39:56 +0500

    RSSEC-241 feat: delete promo requests

--- src/pages/basket/listeners/applyPromoCode.ts
+++ src/pages/basket/listeners/applyPromoCode.ts
@@ -1,12 +1,8 @@
-import { applyPromoToCart } from '../../../server/promo/addPromoApi';
-
-export function applyPromoCode(id: string) {
+export function applyPromoCode() {
     document.addEventListener('click', async (event) => {
         const target = event.target as HTMLButtonElement;
         if (target.className === 'lap-promo-but') {
-            const inputElem = document.getElementById(`${id}-promo-input`) as HTMLInputElement;
-            const code = inputElem.value;
-            await applyPromoToCart(code);
+            console.log(target);
         }
     });
 }
