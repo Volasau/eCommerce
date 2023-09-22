f925b950f9a94b61a4272888fb2765b727c5a04b
Ryhor Volasau <G.volosov@gmail.com>
2023-09-22 11:29:49 +0400

    RSSEC-266 refactory: delet passing array to function

--- src/pages/profile/createElement.ts
+++ src/pages/profile/createElement.ts
@@ -1,16 +1,8 @@
 export function createElement(tagName: string, classNames: string, innerHTML?: string): HTMLElement {
     const element = document.createElement(tagName);
-    if (classNames) {
-        if (Array.isArray(classNames)) {
-            classNames.forEach((className) => element.classList.add(className));
-        } else {
-            element.classList.add(classNames);
-        }
-    }
-
+    element.classList.add(classNames);
     if (innerHTML) {
         element.innerHTML = innerHTML;
     }
-
     return element;
 }
